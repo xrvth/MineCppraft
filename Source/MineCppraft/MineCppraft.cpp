d34fcc2e42f00c4ef616676e77aefdeb3d96720d
Julia Ziemba <40124487+xrvth@users.noreply.github.com>
2021-05-07 20:34:22 +0200

    source from unreal

---
+++ Source/MineCppraft/MineCppraft.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MineCppraft.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MineCppraft, "MineCppraft" );
+ 
